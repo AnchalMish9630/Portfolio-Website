3435d9be58578d7639a701532d8410755cdce3aa
AnchalMish9630 <anchalmish2025@gmail.com>
2025-05-19 02:11:02 +0530

    added scorllbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
 import { FiX, FiMenu } from "react-icons/fi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { useFormState } from "react-dom";
+import { useEffect } from "react";
 
 export const Header = () => {
 
-  const [clicked] = useState(false);
+  const [clicked, setIsClicked] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   const [currentTab, setCurrentTab] = useState("");
   const handleNavBar = (currentId)=>{
+    
     setCurrentTab(currentId);
+    setIsClicked(false)
   }
+
+  // Detect scroll and change navbar background
+  // .......... how scroll is handled ?..............// 
+  // First we want to add event listener to scroll event, and what function fill be called on scoll
+  // we will pass a function to it   // i.e => handleScroll 
+  // on every scroll that function will be called, will check that if scrollY's length is greater than
+  // 50px or not , if yes set Scrolled as true else false
+  // on  Scrolled as true, Nav bar css's opacity and other things will be changed 
+  // bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md
+
+  useEffect(()=>{
+    const handleScrollbar = ()=>{
+      setIsScrolled(window.scrollY > 60)
+    }
+    window.addEventListener("scroll", handleScrollbar);
+    return ()=> window.removeEventListener("scroll", handleScrollbar)
+  },[])  // putting funtion inside useEffect because it will execute after component renders and when out state variable changes and component render then it will run
+
   const navItems=[
     {id:"about", label: "About"},
     {id:"skills", label: "Skills"},
@@ -19,7 +40,8 @@ export const Header = () => {
   ]
 
   return (
-    <nav className="bg-transparent">
+    <nav className={`
+     ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"}`}>
       <div className="text-white py-5 flex justify-between items-center">
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
@@ -30,16 +52,17 @@ export const Header = () => {
         </div>
         <ul className="md:flex space-x-8 text-gray-300">
           {
-            navItems.map((navItem)=>{
+          navItems.map((navItem)=>{
+            return (
               <li key={navItem.id}
-              className={`cursor-pointer hover:text-[#8245ec]
-              ${currentTab === navItem.id ? "text-[#8245ec]" : ""}}`}>
-                  <button onClick={()=>handleNavBar(navItem.id)}>
-                    {navItem.label}
-                  </button>
+                className={`cursor-pointer hover:text-[#8245ec] ${currentTab === navItem.id ? "text-[#8245ec]" : ""}`}
+              >
+                <button onClick={() => handleNavBar(navItem.id)}>
+                  {navItem.label}
+                </button>
               </li>
-            })
-          }
+            );
+          })}
         </ul>
       </div>
     </nav>
